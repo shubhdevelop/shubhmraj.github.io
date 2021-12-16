7e4fe3945f22e91ab2067c7f6fae6268f622f0dc
shubham raj <shubhamxraj10@gmail.com>
2021-12-16 15:12:06 +0530

    Updates

---
+++ component---src-pages-index-js-c0513c01a92f17185aea.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[678],{704:function(e,l,n){n.r(l);var t=n(294);l.default=function(){return t.createElement("main",null,t.createElement("h1",null,"hello "))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-c0513c01a92f17185aea.js.map
